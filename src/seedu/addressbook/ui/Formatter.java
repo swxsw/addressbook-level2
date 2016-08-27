b1d6acec44b9425fad77af6cce0f369da54bb5fb
swxsw <suwei9@gmail.com>
2016-08-27 23:27:43 +0800

    Extracted formatting responsibilities from TextUi into Formatter class
    Created Formatter class to contatin getIndexedListItem and
    getIndexedListForViewing extracted from TextUi

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,33 @@
+package seedu.addressbook.ui;
+
+import java.util.List;
+
+public class Formatter {
+    
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+    
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+    
+    /** Formats a list of strings as a viewable indexed list. */
+    protected static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    protected static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+}
