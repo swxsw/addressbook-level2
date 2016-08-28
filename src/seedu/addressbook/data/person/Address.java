888fd8f7e577ba6a46da51e128bc925b593d630f
swxsw <suwei9@gmail.com>
2016-08-28 21:37:55 +0800

    Changed method in Address and other parts of code to work with the 4 new classes
    Code now works with Block, Unit, Street, PostalCode class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -1,5 +1,8 @@
 package seedu.addressbook.data.person;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
@@ -9,11 +12,16 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Address {
 
     public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can only be in a/BLOCK, STREET, UNIT, POSTAL_CODE format";
+    public static final String ADDRESS_VALIDATION_REGEX = "(.+),(.+),(.+),(.+)";
 
-    public final String value;
+    //public final String value;
     private boolean isPrivate;
+    
+    public final Block blockData;
+    public final Street streetData;
+    public final Unit unitData;
+    public final PostalCode postalCodeData;
 
     /**
      * Validates given address.
@@ -25,7 +33,20 @@ public class Address {
         if (!isValidAddress(address)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = address;
+        //this.value = address;
+        
+        Pattern addressPattern = Pattern.compile(ADDRESS_VALIDATION_REGEX);
+        
+        Matcher addressMatcher = addressPattern.matcher(address);
+        
+        if(addressMatcher.find()){
+            this.blockData = new Block(addressMatcher.group(1));
+            this.streetData = new Street(addressMatcher.group(2));
+            this.unitData = new Unit(addressMatcher.group(3));
+            this.postalCodeData = new PostalCode(addressMatcher.group(4));
+        } else {
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
     }
 
     /**
@@ -37,19 +58,20 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return blockData.getBlock() + " " + streetData.getStreet() + " " + unitData.getUnit() + " " + postalCodeData.getPostalCode();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.toString().equals(((Address) other).toString())); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return (blockData.getBlock() + " " + streetData.getStreet() + " " 
+                + unitData.getUnit() + " " + postalCodeData.getPostalCode()).hashCode();
     }
 
     public boolean isPrivate() {
@@ -57,14 +79,48 @@ public class Address {
     }
 }
 
-Class Block{
+/**
+ * Subclasses of the address class.
+ * @author sw
+ *
+ */
+
+class Block{
+    private String blockData;
+    public Block(String data){
+        this.blockData = data;
+    }
+    public String getBlock(){
+        return this.blockData;
+    }
 }
 
-Class Street{
+class Street{
+    private String streetData;
+    public Street(String data){
+        this.streetData = data;
+    }
+    public String getStreet(){
+        return this.streetData;
+    }
 }
 
-CLass Unit{
+class Unit{
+    private String unitData;
+    public Unit(String data){
+        this.unitData = data;
+    }
+    public String getUnit(){
+        return this.unitData;
+    }
 }
 
-Class PostalCode{
+class PostalCode{
+    private String postalCodeData;
+    public PostalCode(String data){
+        this.postalCodeData = data;
+    }
+    public String getPostalCode(){
+        return this.postalCodeData;
+    }
 }
